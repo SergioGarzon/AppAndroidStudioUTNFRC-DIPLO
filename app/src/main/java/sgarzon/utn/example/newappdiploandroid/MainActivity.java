95d7445b6d00b0e2282e0f8effaef77f62db5d0d
Sergio Garzon <sggarzonutn@gmail.com>
2021-02-19 02:35:30 -0300

    New Commit

--- app/src/main/java/sgarzon/utn/example/newappdiploandroid/MainActivity.java
+++ app/src/main/java/sgarzon/utn/example/newappdiploandroid/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //Nueva aplicación para la clase 2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
